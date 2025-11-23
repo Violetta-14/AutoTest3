d9a1b3cff0e318f3f998152e174b02e980368b9c
Violetta <violetta_stukach@mail.ru>
2025-11-23 17:22:25 +0300

    Add new class SimpleNumber

---
+++ src/main/java/org/example/SimpleNumber.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class SimpleNumber {
+
+    private int num;
+
+    public SimpleNumber (int num){
+        this.num = num;
+    }
+
+    public boolean isPositive(){
+        return num > 0;
+    }
+}
